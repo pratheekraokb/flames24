a6e0f56ad8989ee295c4671803b7cce296af6b28
= <pratheekmulleria@gmail.com>
2024-02-12 21:50:55 +0530

    Images tested

--- flames24_app/urls.py
+++ flames24_app/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 
 from flames24_app import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
