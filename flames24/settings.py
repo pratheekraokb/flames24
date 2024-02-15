84ae01b8846b0a3b02f87e1e121be80bdc7a31e0
= <pratheekmulleria@gmail.com>
2024-02-15 21:55:50 +0530

    db version changed

--- flames24/settings.py
+++ flames24/settings.py
@@ -107,9 +107,7 @@ DATABASES = {
         'PASSWORD': 'vrRagtU2AE0L',
         'HOST': 'ep-young-salad-a4jyz6fk-pooler.us-east-1.aws.neon.tech',  # Or the IP address of your PostgreSQL server
         'PORT': '',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        
     }
 }
 
